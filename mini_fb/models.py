6bb30b3647f256432f100576fd159ada9466db1f
starwarsman715 <starwarsman715@gmail.com>
2024-10-28 12:39:20 -0400

    added friends

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -23,6 +23,25 @@ class Profile(models.Model):
     def get_absolute_url(self):
         '''Return the URL to access a particular profile instance.'''
         return reverse('show_profile', kwargs={'pk': self.pk})
+    
+    def get_friends(self):
+        '''Return a list of all friends' profiles for this profile.'''
+        # Get all friendship objects where this profile is either profile1 or profile2
+        friends1 = Friend.objects.filter(profile1=self)
+        friends2 = Friend.objects.filter(profile2=self)
+    
+        # Create a list of friend profiles
+        friend_profiles = []
+    
+        # Add profile2s from friends1
+        for friendship in friends1:
+            friend_profiles.append(friendship.profile2)
+        
+        # Add profile1s from friends2
+        for friendship in friends2:
+            friend_profiles.append(friendship.profile1)
+        
+        return friend_profiles
 
 class StatusMessage(models.Model):
     '''Model representing a user's status message.'''
@@ -46,3 +65,13 @@ class Image(models.Model):
 
     def __str__(self):
         return f"Image for StatusMessage ID {self.status_message.id} uploaded at {self.uploaded_at}"
+
+class Friend(models.Model):
+    '''Encapsulate the idea of a friendship between two profiles.'''
+    profile1 = models.ForeignKey(Profile, related_name='profile1', on_delete=models.CASCADE)
+    profile2 = models.ForeignKey(Profile, related_name='profile2', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        '''Return a string representation of this friendship.'''
+        return f'{self.profile1.first_name} {self.profile1.last_name} & {self.profile2.first_name} {self.profile2.last_name}'
