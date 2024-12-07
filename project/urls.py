416e352f95c9db8b630e13ea839ee7490587917e
starwarsman715 <starwarsman715@gmail.com>
2024-12-06 19:42:45 -0500

    added song and user detail views

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,10 @@ app_name = 'project'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('users/', UserListView.as_view(), name='user_list'),
     path('songs/', SongListView.as_view(), name='song_list'),
+    
+    path('users/<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
+    path('songs/<int:pk>/', views.SongDetailView.as_view(), name='song_detail'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,7 +1,8 @@
 # project/views.py
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import User, Song
+from django.views.generic import ListView, DetailView
+from .models import User, Song, UserGenre, UserSong
+
 
 def home(request):
     return render(request, 'project/home.html')
@@ -10,10 +11,40 @@ class UserListView(ListView):
     model = User
     template_name = 'project/user_list.html'  # Specify your template name
     context_object_name = 'users'             # Name of the list in the template
-    paginate_by = 8                           # Optional: paginate by 10 users per page
+    paginate_by = 6                           # Optional: paginate by 10 users per page
 
 class SongListView(ListView):
     model = Song
     template_name = 'project/song_list.html'  # Specify your template name
     context_object_name = 'songs'             # Name of the list in the template
-    paginate_by = 8                           # Optional: paginate by 10 songs per page
+    paginate_by = 6                         # Optional: paginate by 10 songs per page
+
+# project/views.py
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'project/user_detail.html'  # Specify your template
+    context_object_name = 'user_profile'        # Context variable name
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.object
+        # Fetch preferred genres
+        context['preferred_genres'] = UserGenre.objects.filter(user=user).select_related('genre')
+        # Fetch favorite songs
+        context['favorite_songs'] = UserSong.objects.filter(user=user).select_related('song')
+        return context
+
+# project/views.py
+
+class SongDetailView(DetailView):
+    model = Song
+    template_name = 'project/song_detail.html'  # Specify your template
+    context_object_name = 'song'                # Context variable name
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        song = self.object
+        # Fetch users who have this song as a favorite
+        context['favorite_users'] = UserSong.objects.filter(song=song).select_related('user')
+        return context
